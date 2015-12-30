b6c360fbdd12bb15c531f3c97d8a080fd1ec2196
Luke Roberts <email@luke-roberts.co.uk>
2015-12-30 12:57:11 +0000

    tidy up index on tasks page

--- app/admin/task.rb
+++ app/admin/task.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register ::Pushbit::Task do
   menu label: "Tasks"
+  index do
+    id_column
+    column :number
+    column :status
+    column :kind
+    column :created_at
+    column :updated_at
+    column :completed_at
+    column :container_status
+    column :reason
+    actions
+  end
 end
 
